c07bda426e4da1879758fe109252d855249c611b
Khairov Marat <khairovm9975@gmail.com>
2023-10-16 23:38:05 +0300

    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { cn } from '@/lib/utils';
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/toaster';
 
 
 export const metadata = {
@@ -22,9 +23,12 @@ export default function RootLayout({
       inter.className)}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
         <Navbar />
+
         <div className='container max-w-7x1 mx-auto h-full pt-12'>
           {children}
         </div>
+
+        <Toaster />
       </body>
     </html>
   )
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -6,11 +6,13 @@ import { cn } from '@/lib/utils'
 import { useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { Icons } from './Icons'
+import { useToast } from '@/hooks/use-toast'
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement>{}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props}) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
 
   const loginWithGoogle = async () => {
     setIsLoading(true);
@@ -18,7 +20,11 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props}) => {
     try {
       await signIn('google');
     } catch (error) {
-      // toast notification
+      toast({ 
+        title: 'There was a problem',
+        description: 'There was an error logging in with Google',
+        variant: 'destructive'
+      })
     } finally {
       setIsLoading(false);
     }
